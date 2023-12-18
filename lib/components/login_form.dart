7f0ba498b35078d9b4fc9816c6ab7f3b42cd4d92
Wei  Huang <1119765689hw@gmail.com>
2023-12-18 20:04:46 +0800

    because of some causes, I will not go on my development of this InterestMate app, goodbye flutter (#2)
    * finish the logo and logoName animation
    
    * because of some causes, I will not go on my development of this InterestMate app, goodbye flutter

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -21,6 +21,54 @@ class _LoginFormState extends State<LoginForm> {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.only(topLeft: Radius.circular(101.sp)),
             color: Colors.white),
+        child: Padding(
+          padding: EdgeInsets.only(left: 38.w, top: 78.h, right: 44.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(
+                    color: Color(0xFF0B372F),
+                    fontSize: 32.sp,
+                    fontWeight: FontWeight.w800),
+              ),
+              Text(
+                "sign in",
+                style: TextStyle(
+                    color: Color(0xFF0B372F),
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 60.h,
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextField(
+                    cursorColor: Color(0xFF0B372F),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.phone_android,
+                        color: Color(0xFF979797),
+                      ),
+                      labelText: "手机号",
+                      labelStyle: TextStyle(color: Color(0xFF0B372F)),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.gpp_good_outlined,
+                            color: Color(0xFF979797)),
+                        labelText: "验证码",
+                        labelStyle: TextStyle(color: Color(0xFF0B372F))),
+                  ),
+                ],
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
